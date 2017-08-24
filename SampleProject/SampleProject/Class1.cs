ae819fe1c29953188e438bedeaaed6e92128be70
Unknown <unknown@example.com>
2017-08-24 14:22:01 +0800

    Added new method named Method_Source to Source branch.

--- SampleProject/SampleProject/Class1.cs
+++ SampleProject/SampleProject/Class1.cs
@@ -8,5 +8,10 @@ namespace SampleProject
 {
     public class Class1
     {
+        // This new method named Method_Source 
+        // will just be available in Source branch
+        public void Method_Source() { }
     }
+
+    
 }
